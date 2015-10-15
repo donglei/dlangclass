74084b506be29ae9076c9500ce0d9e9ab4ba3e2d
donglei@donglei.com <donglei@donglei.com>
2015-10-15 11:34:43 +0800

    aa_sorted_sign  aa_sort

---
+++ aa_sort.d
@@ -0,0 +1,42 @@
+import std.algorithm, std.typecons;
+
+ Tuple!(K, V)[] aa_sort(K, V)(V[K] aa){
+     typeof(return) r=[];
+     foreach(k,v;aa) r~=tuple(k,v);
+     sort!q{a[0]<b[0]}(r);
+     return r;
+ }
+ 
+ string aa_sorted_sign(string[string] data, string sign_key = string.init)
+ {
+	import std.stdio, std.uri;
+	auto sortbb= aa_sort(data);
+	string[] need_md5;
+	writeln(sortbb);
+	foreach( cc ; sortbb)
+	{
+		if(cc[0] == "sign")
+		{
+			continue;
+		}
+		need_md5 ~= cc[0] ~ "=" ~ encodeComponent(cc[1]);
+	}
+	import std.digest.md, std.digest.sha, std.array;
+    import std.string: toLower;
+	return toHexString(md5Of(need_md5.join('&') ~ sign_key)).toLower();
+ }
+ 
+
+ void main()
+ {
+	auto bb = [
+		"name":"donglei",
+		"id":"123%?123",
+		"age":"123",
+		"age1":"123"
+	];
+	import std.stdio;
+	writeln(aa_sorted_sign(bb, "123"));
+	writeln(aa_sorted_sign(bb));
+	
+ }
