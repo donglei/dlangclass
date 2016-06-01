f90f0da52e19868aa326b6ee95a6684f3c9aeec8
donglei <xiaosan@outlook.com>
2016-06-01 17:08:54 +0800

    camelCaseToUnderscoreDelimited

--- helper.d
+++ helper.d
@@ -15,3 +15,24 @@ string getYearMonthFormat()
 	auto currentTime = Clock.currTime();
 	return format("%d%02d", currentTime.year, currentTime.month);
 }
+
+/// converts camel case MyEntityName to my_entity_name
+string camelCaseToUnderscoreDelimited(immutable string s) {
+    string res;
+    bool lastLower = false;
+    foreach(ch; s) {
+        if (ch >= 'A' && ch <= 'Z') {
+            if (lastLower) {
+                lastLower = false;
+                res ~= "_";
+            }
+            res ~= std.ascii.toLower(ch);
+        } else if (ch >= 'a' && ch <= 'z') {
+            lastLower = true;
+            res ~= ch;
+        } else {
+            res ~= ch;
+        }
+    }
+    return res;
+}
